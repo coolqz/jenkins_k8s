bfc657d34a956813b78b2ca1b58ccb59f533c7fa
Administrator <admin@example.com>
2022-11-03 17:04:13 +0800

    jenkinsfile create

---
+++ src/org/devops/tools.groovy
@@ -0,0 +1,31 @@
+package org.devops
+
+//writefile
+def writefile(filename,content){                             
+  writefile encoding: 'UTF-8', file: "${filename}", text: "${content}" 
+}
+
+def harborlogin(){
+    withCredentials([
+        usernamePassword(
+            credentialsId: '84d8aa3c-d320-4fa2-ba4d-910894080cf5', 
+            passwordVariable: 'password', 
+            usernameVariable: 'username'
+        )
+    ]) 
+    sh """
+        docker login 192.168.100.203 -u $username -p $password
+    """
+}
+
+def servicedeploy(){
+    kubeconfig(
+        credentialsId: 'f2c47258-5493-428f-a102-c6ebaa012ff3', 
+        serverUrl: 'https://192.168.100.10:6443'
+        ) 
+    {
+        sh """
+            kubectl apply -f javademo.yaml
+        """
+    }
+}
---
+++ var/javademo.groovy
@@ -0,0 +1,78 @@
+def call{
+    def tools = new org.deveops.tools()
+    def requestdockerfile = libraryResource 'org/javademo/dockerfile/dockerfile'
+    def requestyaml = libraryResource 'org/javademo/yaml/javademo.yaml'
+
+    pipeline {
+        agent {
+            kubernetes {
+                label "jenkins-slave"
+                yaml '''
+        apiVersion: v1
+        kind: Pod
+        metadata:
+            name: jenkins-slave
+        spec:
+          containers:
+          - name: jnlp
+            image: "192.168.100.203/library/jenkins-jnlp-slave:maven"
+            volumeMounts:
+              - name: docker-cmd
+                mountPath: /usr/bin/docker
+              - name: docker-sock
+                mountPath: /var/run/docker.sock
+          volumes:
+            - name: docker-cmd
+              hostPath: /usr/bin/docker
+            - name: docker-sock
+              hostPath: /var/run/docker.sock
+        '''
+            }
+       }
+ 
+        stages {
+            stage('checkout_code'){
+                steps {
+                    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/master']],
+                        extensions: [],
+                        userRemoteConfigs: [[
+                            credentialsId: '46bc0911-8468-4171-b347-aaad153d5111', 
+                            url: 'http://192.168.100.200/test/javademo.git'
+                            ]]
+                        ])
+                }
+            }
+
+            stage('maven_build'){
+                steps {
+                    mvn clean package -Dmaven.test.skip=true
+                }
+            }
+
+            stage('image_build'){
+                steps {
+                    script{
+                        tools.writefile('dockerfile', requestdockerfile)
+                        tools.harborlogin()
+                    }
+                    sh """
+                        docker build -t javademo:v1 .
+                        docker tag javademo:v1 192.168.100.203/test/javademo:v1
+                        docker push 192.168.100.203/test/javademo:v1
+                    """
+                }
+            }
+
+            stage('service_deploy'){
+                steps {
+                    script{
+                        tools.writefile('javademo.yaml', requestyaml)
+                        tools.servicedeploy()
+                    }                   
+                }
+            }
+        }
+    }
+}
