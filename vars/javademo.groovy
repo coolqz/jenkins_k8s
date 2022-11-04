ed32e5790fbae61cfedee1ca06893c6796e03bae
Administrator <admin@example.com>
2022-11-04 17:04:44 +0800

    update

--- vars/javademo.groovy
+++ vars/javademo.groovy
@@ -15,7 +15,10 @@ def call(){
         spec:
           containers:
           - name: jnlp
-            image: "192.168.100.203/library/jnlp-slave-maven:3.8.6"
+            image: "192.168.100.203/library/jenkins-jnlp-slave:jdk11"
+
+          - name: maven
+            image: "192.168.100.203/library/maven:3.8.6"
             volumeMounts:
               - name: docker-cmd
                 mountPath: /usr/bin/docker
@@ -40,45 +43,53 @@ def call(){
         stages {
             stage('checkout_code'){
                 steps {
-                    checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: '*/master']],
-                        extensions: [],
-                        userRemoteConfigs: [[
-                            credentialsId: '46bc0911-8468-4171-b347-aaad153d5111', 
-                            url: 'http://192.168.100.200/test/javademo.git'
-                        ]]
-                    ])
+                    containers(name: 'maven'){
+                        checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: '*/master']],
+                            extensions: [],
+                            userRemoteConfigs: [[
+                                credentialsId: '46bc0911-8468-4171-b347-aaad153d5111', 
+                                url: 'http://192.168.100.200/test/javademo.git'
+                            ]]
+                        ])
+                    }
                 }
             }
 
             stage('maven_build'){
                 steps {
-                    sh """
-                        mvn clean package -Dmaven.test.skip=true
-                    """
+                    containers(name: 'maven') {
+                        sh """
+                            mvn clean package -Dmaven.test.skip=true
+                        """
+                    }
                 }
             }
 
             stage('image_build'){
                 steps {
-                    script{
-                        tools.writefile('dockerfile', requestdockerfile)
-                        tools.harborlogin()
+                    containers(name: 'maven') {
+                        script{
+                            tools.writefile('dockerfile', requestdockerfile)
+                            tools.harborlogin()
+                        }
+                        sh """
+                            docker build -t javademo:v1 .
+                            docker tag javademo:v1 192.168.100.203/test/javademo:v1
+                            docker push 192.168.100.203/test/javademo:v1
+                        """
                     }
-                    sh """
-                        docker build -t javademo:v1 .
-                        docker tag javademo:v1 192.168.100.203/test/javademo:v1
-                        docker push 192.168.100.203/test/javademo:v1
-                    """
                 }
             }
 
             stage('service_deploy'){
                 steps {
-                    script{
-                        tools.writefile('javademo.yaml', requestyaml)
-                        tools.servicedeploy()
+                    containers(name: 'maven') {
+                        script{
+                            tools.writefile('javademo.yaml', requestyaml)
+                            tools.servicedeploy()
+                        }
                     }                   
                 }
             }
